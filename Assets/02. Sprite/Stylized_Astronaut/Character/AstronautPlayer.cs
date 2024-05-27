248da5e9ded41e1b4a15e8ca039bf329b723d2c1
minho0516 <pmhminho0516@gmail.com>
2024-05-27 19:45:09 +0900

    d
    d

--- Assets/02. Sprite/Stylized_Astronaut/Character/AstronautPlayer.cs
+++ Assets/02. Sprite/Stylized_Astronaut/Character/AstronautPlayer.cs
@@ -1,129 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 namespace AstronautPlayer
 {
-    public class AstronautPlayer : MonoBehaviour
-    {
-        private Animator anim;
-        private CharacterController controller;
 
-        public float speed = 6.0f;
-        public float turnSpeed = 400.0f;
-        private Vector3 moveDirection = Vector3.zero;
-        public float gravity = 20.0f;
-        [SerializeField] private float jumpForce = 8f;
+	public class AstronautPlayer : MonoBehaviour {
 
-        private bool isJumping = false;
+		private Animator anim;
+		private CharacterController controller;
 
-        [Header("Camera")]
-        [SerializeField] private float looksensitivity;
-        [SerializeField] private float cameraRotationLimit;
-        private float currentCameraRotationX;
-        [SerializeField] private Camera theCamera;
+		public float speed = 600.0f;
+		public float turnSpeed = 400.0f;
+		private Vector3 moveDirection = Vector3.zero;
+		public float gravity = 20.0f;
 
-        void Start()
-        {
-            controller = GetComponent<CharacterController>();
-            anim = gameObject.GetComponentInChildren<Animator>();
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
+		void Start () {
+			controller = GetComponent <CharacterController>();
+			anim = gameObject.GetComponentInChildren<Animator>();
+		}
 
-        void Update()
-        {
-            Move();
-            CameraRotation();
-            CharacterRotation();
-            CheckJump();
-            ApplyGravity();
-            MoveCharacter();
-            ResetJumpIfNeeded();
-        }
+		void Update (){
+			if (Input.GetKey ("w")) {
+				anim.SetInteger ("AnimationPar", 1);
+			}  else {
+				anim.SetInteger ("AnimationPar", 0);
+			}
 
-        private void Move()
-        {
-            float verticalInput = Input.GetAxis("Vertical");
+			if(controller.isGrounded){
+				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+			}
 
-            if (verticalInput != 0)
-            {
-                anim.SetInteger("AnimationPar", 1);
-            }
-            else
-            {
-                anim.SetInteger("AnimationPar", 0);
-            }
-
-            if (controller.isGrounded)
-            {
-                moveDirection = transform.forward * verticalInput * speed;
-            }
-
-            float horizontalInput = Input.GetAxis("Horizontal");
-            transform.Rotate(0, horizontalInput * turnSpeed * Time.deltaTime, 0);
-        }
-
-        private void CameraRotation()
-        {
-            // 상하 카메라 회전
-            float xRotation = Input.GetAxisRaw("Mouse Y");
-            float cameraRotationX = xRotation * looksensitivity;
-            currentCameraRotationX -= cameraRotationX;
-            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
-
-            theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
-        }
-
-        private void CharacterRotation()
-        {
-            // 좌우 카메라 회전
-            float yRotation = Input.GetAxisRaw("Mouse X");
-            Vector3 characterRotationY = new Vector3(0f, yRotation, 0f) * looksensitivity;
-            transform.Rotate(characterRotationY);
-        }
-
-        private void CheckJump()
-        {
-            // 캐릭터가 바닥에 있으면 점프 가능
-            if (controller.isGrounded)
-            {
-                // 점프 키를 누르면 점프
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    isJumping = true;
-                    moveDirection.y = jumpForce;
-
-                }
-            }
-        }
-
-        private void ApplyGravity()
-        {
-            // 캐릭터가 바닥에 닿아 있지 않으면 중력 적용
-            if (!controller.isGrounded)
-            {
-                moveDirection.y -= gravity * Time.deltaTime;
-            }
-        }
-
-        private void MoveCharacter()
-        {
-            // 이동 방향 적용
-            controller.Move(moveDirection * Time.deltaTime);
-        }
-
-        private void ResetJumpIfNeeded()
-        {
-            // 캐릭터가 바닥에 닿아 있으면 점프 상태 초기화
-            if (controller.isGrounded && !isJumping)
-            {
-                moveDirection.y = 0f;
-            }
-
-            // 점프가 완료되면 상태 초기화
-            if (isJumping && controller.isGrounded)
-            {
-                isJumping = false;
-            }
-        }
-    }
+			float turn = Input.GetAxis("Horizontal");
+			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+			controller.Move(moveDirection * Time.deltaTime);
+			moveDirection.y -= gravity * Time.deltaTime;
+		}
+	}
 }
